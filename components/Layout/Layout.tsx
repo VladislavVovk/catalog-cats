66456cd0b8ae3edd387081e95e343c9768614a66
Vladislav <Timba20@yandex.ru>
2022-01-15 19:41:10 +0300

    add keywords

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,6 +13,7 @@ const Layout = ({ children }: Props) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Cat breeds: Discover 78 of the most beautiful, elegant and unique cat breeds. This complete and detailed list will help you choose your puurfect best friend" />
+      <meta name="keywords" content="cat breeds, cat breeding, happy cat large breed,cat, породы кошек, породы кошек фото, породы кошек +с названиями, самые породы кошек, описание пород кошек" />
       <meta property="og:title" content="Cat breeds" />
       <meta property="og:description" content="Discover 78 of the most beautiful, elegant and unique cat breeds. This complete and detailed list will help you choose your puurfect best friend"></meta>
       <meta property="og:type" content="website" />
