580c2881416df55b5e9a3ea3e622cbbe851b02ff
Vladislav <Timba20@yandex.ru>
2022-01-15 17:49:21 +0300

    add description page for kitty

--- pages/breeds/[breed].tsx
+++ pages/breeds/[breed].tsx
@@ -7,6 +7,9 @@ import {
   IconButton,
   Link,
   Heading,
+  Stack,
+  Image,
+  Text,
   useColorModeValue
 } from '@chakra-ui/react'
 
@@ -14,7 +17,7 @@ import { ArrowBackIcon } from '@chakra-ui/icons'
 import { getListBreed } from '../../api/index'
 
 const Breed = ({ cats }) => {
-
+  console.log(cats)
   return (
     <Layout>
       <motion.div
@@ -25,15 +28,56 @@ const Breed = ({ cats }) => {
         <Box mt={20} width={"100%"}>
           <Link href="/">
             <IconButton
-            transition="all 0.5s"
-            color={useColorModeValue('black', 'white')}
-            _hover={{
-              color: useColorModeValue('purple', 'orange')
-            }}
-             variant="text" aria-label={"back"} icon={<ArrowBackIcon fontSize={60} />} />
+              transition="all 0.5s"
+              color={useColorModeValue('black', 'white')}
+              _hover={{
+                color: useColorModeValue('purple', 'orange')
+              }}
+              variant="text" aria-label={"back"} icon={<ArrowBackIcon fontSize={60} />} />
           </Link>
         </Box>
-        <Heading>{cats.name}</Heading>
+        <Box>
+          <Stack
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 5, md: 15 }}
+            width={{ base: '100%', md: '100%' }}
+            direction={{ base: 'column', md: 'row' }}
+            key={cats.id}
+          >
+            <Box
+              width={{ base: '100%', sm: '85%' }}
+              zIndex="2"
+              marginLeft={{ base: '0', sm: '5%' }}
+              marginTop="5%"
+            >
+              <Image
+                alt={cats.slug}
+                borderRadius="lg"
+                float={"left"}
+                objectFit={"contain"}
+                w={'100%'}
+                h={'auto'}
+                sx={{ marginRight: 10, marginBottom: 5 }}
+                minWidth={{ base: '100%', md: "370px" }}
+                maxWidth={{ base: '100%', md: "370px" }}
+                src={cats.image_url}
+              />
+              <Heading
+                width={"100%"}
+
+                fontSize={{ base: '3xl', sm: '3xl', md: '3xl', lg: '4xl' }}
+                textAlign={{ base: "center", md: "left" }}
+              >
+                {cats.name}
+              </Heading>
+              <Text mt={10}>
+                <strong>Описание: </strong>
+                {cats.description}
+              </Text>
+
+            </Box>
+          </Stack>
+        </Box>
       </motion.div>
     </Layout>
   )
@@ -47,7 +91,7 @@ export const getServerSideProps = async (context) => {
   })
   const cats = res
   return {
-    props: {cats}
+    props: { cats }
   }
 
 }
